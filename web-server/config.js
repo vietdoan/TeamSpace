1415c140e802a128f81b0f7457a43238c8dbfd03
vietdoan <vietdoanhp1996@gmail.com>
2016-11-23 18:02:01 +0700

    add config.js

---
+++ web-server/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'secretKey': '12345-67890-09876-54321',
+  'mongoUrl': 'mongodb://localhost:27017/TeamSpace',
+  'facebook': {
+    clientID: '1230716577011058',
+    clientSecret: 'd167a58996c31730bbba2230e7441c8d',
+    callbackURL: 'https://localhost:3443/users/facebook/callback'
+  }
+}
